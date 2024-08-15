27247e568dda945c502f99a5ef7b6d1fff8e3dfe
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2024-08-15 11:52:10 -0300

    Menu de inicio Primeras piezas

--- chess.cpp
+++ chess.cpp
@@ -4,49 +4,86 @@
 
 #include <ncurses.h>
 
-void options(int key_pressed){
-    switch (key_pressed)
-    {
-    case 27:
-        endwin();
-        break;
-    default:
-        break;
-    }
-}
-
-
-
 int main() {
     
     int i, j, k, l, key;
     const int tablero = 8;
-    bool running = true;
+    bool menu = true;
+    bool jugando = false;
 
     initscr();
+    curs_set(0);
+    keypad(stdscr, TRUE);
 
-
-    while(running){
-        key = getch();
+    while(menu){
+        attroff(A_REVERSE);
+        clear();
         refresh();
-        options(key);
+        printw("Presiona [ENTER] para comenzar a jugar");
+        refresh();
 
-        for(i = 0; i < tablero; i++) {
-            for(j = 0; j < tablero; j++) {
-                if((i+j) % 2 == 0) {
-                    attron(A_REVERSE);
-                } else {
-                    attroff(A_REVERSE);
-                }
-                for(k = 0; k < 3; k++) {
-                    move((3*i)+k, 5*j);
-                    for(l = 0; l < 5; l++) {
-                        printw(" ");
+        key = getch();
+        switch (key)
+        {
+        case 27:
+            endwin();
+            menu = false;
+            break;
+        case 13:
+            jugando = true;
+            clear();
+            break;
+        case 10:
+            jugando = true;
+            clear();
+            break;
+        default:
+            break;
+        }
+
+        while(jugando){
+
+            for(i = 0; i < tablero; i++) {
+                for(j = 0; j < tablero; j++) {
+                    if((i+j) % 2 == 0) {
+                        attron(A_REVERSE);
+                    } else {
+                        attroff(A_REVERSE);
+                    }
+                    for(k = 0; k < 3; k++) {
+                        move((3*i)+k, 5*j);
+                        for(l = 0; l < 5; l++) {
+                            printw(" ");
+                        }
                     }
                 }
             }
+
+            move(1,2);
+            printw("0");
+            
+            
+
+            key = getch();
+            switch (key)
+            {
+            case 27:
+                endwin();
+                jugando = false;
+                break;
+            case 13:
+                jugando = true;
+                clear();
+                break;
+            case 10:
+                jugando = true;
+                clear();
+                break;
+            default:
+                break;
+            }
+
         }
-        printw("\n");
     }
     return 0;
 }
