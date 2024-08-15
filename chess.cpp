b03240fa82e329c864ce1f55313768f5b10c642d
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2024-08-15 19:34:50 -0300

    Avances en el selector de el dibujado de piezas

--- chess.cpp
+++ chess.cpp
@@ -11,23 +11,27 @@ int posy = 0;
 const int tablero = 8;
 bool menu = true;
 bool jugando = false;
+bool hover = false;
 
-typedef struct
-{
+struct Pieza{
     int x;
     int y;
     bool seleccionada = false;
     char figura;
-}Pieza;
-Pieza torre;
-torre.x = 0;
-torre.y = 0;
-torre.figura = 'T';
+};
+
+void dibujar_pieza(Pieza pieza, int ty, int tx, int by, int bx){
+    if(pieza.x == tx && pieza.y == ty && bx == 1 && by == 2){
+        printw("%c", pieza.figura);
+    }
+}
 
 int main() {
 
     initscr();
     keypad(stdscr, TRUE);
+    Pieza torre;
+    torre.x = 0; torre.y = 0; torre.figura = 'T';
 
     while(menu){
         attroff(A_REVERSE);
@@ -59,6 +63,7 @@ int main() {
 
         while(jugando){
 
+            //---------------RENDERIZADO DEL TABLERO---------------//
             for(i = 0; i < tablero; i++) {
                 for(j = 0; j < tablero; j++) {
                     if((i+j) % 2 == 0) {
@@ -68,24 +73,32 @@ int main() {
                     }
                     for(k = 0; k < 3; k++) {
                         move((3*i)+k, 5*j);
-                        for(l = 0; l < 5; l++) {
-                            if (k == 1 && l == 2 && (posy == i || posx == j)){
-                                printw(".");
+                       for(l = 0; l < 5; l++) {
+                            if (posy == i && posx == j && k%2 == 0 && l%2 != 0){           //dibuja las esquinas del selector
+                                if(hover){printw("+");}else{printw("-");}
                             }else{
+                            if (hover && k == 1 && l == 2 && (posy == i || posx == j)){    //sino, dibuja espacios vacíos
+                                printw("."); 
+                            }else{
+                            dibujar_pieza(torre, i, j, l, k);
                             printw(" ");
-                            }
+                            }}
                         }
                     }
                 }
             }
-            move((1+(posy*3)),(2+(posx*5)));
+
+            /*move((1+(posy*3)),(2+(posx*5)));
             printw("%c", torre.figura);
-            attroff(A_REVERSE);
+            */
             curs_set(1);
+            attroff(A_REVERSE);
             move(25,0);
             printw("[ESC] Para salir.\n[FLECHAS] Para moverse.\n[ENTER] Para seleccionar una pieza.");
             key = getch();
 
+
+            //---------------INPUT USUARIO---------------//
             switch (key)
             {
             case 49: case 50: case 51: case 52:
@@ -98,28 +111,22 @@ int main() {
                 endwin();
                 jugando = false;
                 break;
-            case 13:
+            case 13: case 10:
                 jugando = true;
-                clear();
-                break;
-            case 10:
-                jugando = true;
+                hover = !hover;
                 clear();
                 break;
             case KEY_LEFT:
-                if (posx > 0){
-                posx -= 1;}
+                if (posx > 0){posx -= 1;}
                 break;
             case KEY_RIGHT:
                 if (posx<7){posx += 1;}
                 break;
             case KEY_UP:
-                if (posy > 0){
-                    posy -= 1;}
+                if (posy > 0){posy -= 1;}
                 break;
             case KEY_DOWN:
-                if (posy<7){
-                    posy += 1;}
+                if (posy<7){posy += 1;}
                 break;
             default:
                 break;
